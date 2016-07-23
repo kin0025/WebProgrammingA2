4916bfe79cdc846175cd4ce89496d6a0b53371e7
kin0025 <alex@akinrosssmith.id.au>
2016-07-23 12:51:55 +1000

    Initial commit

---
+++ about.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: kin0025
+ * Date: 22/07/2016
+ * Time: 1:03 PM
+ */
---
+++ index.php
@@ -0,0 +1,34 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: kin0025
+ * Date: 20/07/2016
+ * Time: 10:37 PM
+ */
+?>
+
+
+<html lang="en">
+<head>
+    <meta http-equiv="Content-Type" content="text/html;charset=UTF-8">
+    <title>Document</title>
+    <link rel="stylesheet" type=text/css href="style.css">
+</head>
+<header>
+    <nav>
+        <ul>
+            <li>
+                <a href="index.php"><span> Home</span></a>
+            </li>
+            <li>
+                <a href="about.php"><span>About</span></a>
+            </li>
+        </ul>
+    </nav>
+</header>
+<body>
+<div id="content-wrapper">
+Look! Something something something something look over there -->
+</div>
+</body>
+</html>
