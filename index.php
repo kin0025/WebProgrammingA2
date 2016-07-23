ff8f6d80ba3d72d27bb3568ec18d236dcb5c8d6c
kin0025 <alex@akinrosssmith.id.au>
2016-07-23 13:49:32 +1000

    Some css for responsive stuff

--- index.php
+++ index.php
@@ -11,8 +11,11 @@
 <html lang="en">
 <head>
     <meta http-equiv="Content-Type" content="text/html;charset=UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
     <link rel="stylesheet" type=text/css href="style.css">
+    <!--Google font file -->
+    <link href='https://fonts.googleapis.com/css?family=Roboto' rel='stylesheet' type='text/css'>
 </head>
 <header>
     <nav>
@@ -27,8 +30,20 @@
     </nav>
 </header>
 <body>
-<div id="content-wrapper">
-Look! Something something something something look over there -->
+<div class="column-1 border-wrapper"></div>
+<div id="column-6 content-wrapper">
+    <div id="main-body" class="h-panel">
+        <div class="column-8">Look! Something something something something look over there --></div>
+        <div class="column-4">Content</div>
+        <div class="column-4">Content Column 2</div>
+    </div>
+
+
+    <div id="footer-content" class="h-panel">
+
+    </div>
+</div>
+<div class="column-1 border-wrapper"></div>
 </div>
 </body>
 </html>
