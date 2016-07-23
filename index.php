2e21a2a62803e7c48d00e76981d9a49246538575
kin0025 <alex@akinrosssmith.id.au>
2016-07-23 14:11:03 +1000

    Working on the nav styling

--- index.php
+++ index.php
@@ -17,6 +17,9 @@
     <!--Google font file -->
     <link href='https://fonts.googleapis.com/css?family=Roboto' rel='stylesheet' type='text/css'>
 </head>
+
+<body>
+<div class="column-1 border-wrapper"></div>
 <header>
     <nav>
         <ul>
@@ -29,21 +32,29 @@
         </ul>
     </nav>
 </header>
-<body>
-<div class="column-1 border-wrapper"></div>
-<div id="column-6 content-wrapper">
+
+<div id="content-wrapper" class="column-6">
     <div id="main-body" class="h-panel">
         <div class="column-8">Look! Something something something something look over there --></div>
         <div class="column-4">Content</div>
         <div class="column-4">Content Column 2</div>
     </div>
-
-
-    <div id="footer-content" class="h-panel">
-
-    </div>
 </div>
 <div class="column-1 border-wrapper"></div>
-</div>
+<footer id="footer-content" class="h-panel">
+    <div id="footer-links" class="column-3">
+        <ul>
+            <li>
+                <a href="index.php">Home</a>
+            </li>
+            <li>
+                <a href="about.php">About</a>
+            </li>
+        </ul>
+    </div>
+    <div class="column-5">
+        IDK Some content
+    </div>
+</footer>
 </body>
 </html>
