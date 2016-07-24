398cb99d5b30d9b3e2e39ad47c6f0d87de9f7e14
kin0025 <alex@akinrosssmith.id.au>
2016-07-24 23:27:15 +1000

    Fixed page titles

--- about.php
+++ about.php
@@ -7,7 +7,14 @@
  */
 ?>
 <body>
-
+<head>
+    <meta http-equiv="Content-Type" content="text/html;charset=UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>About</title>
+    <link rel="stylesheet" type=text/css href="resources/styles/style.css">
+    <!--Google font file -->
+    <link href='https://fonts.googleapis.com/css?family=Roboto' rel='stylesheet' type='text/css'>
+</head>
 
 <header class="h-panel drop-shadow-bottom">
     <div class="container side-padding ">
--- index.php
+++ index.php
@@ -12,7 +12,7 @@
 <head>
     <meta http-equiv="Content-Type" content="text/html;charset=UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Home</title>
     <link rel="stylesheet" type=text/css href="resources/styles/style.css">
     <!--Google font file -->
     <link href='https://fonts.googleapis.com/css?family=Roboto' rel='stylesheet' type='text/css'>
