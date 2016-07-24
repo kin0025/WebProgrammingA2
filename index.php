d4a33db9614b62337dfbe115d372628719102745
kin0025 <alex@akinrosssmith.id.au>
2016-07-24 14:03:11 +1000

    Added mixins

--- index.php
+++ index.php
@@ -13,7 +13,7 @@
     <meta http-equiv="Content-Type" content="text/html;charset=UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
-    <link rel="stylesheet" type=text/css href="style.css">
+    <link rel="stylesheet" type=text/css href="resources/styles/style.css">
     <!--Google font file -->
     <link href='https://fonts.googleapis.com/css?family=Roboto' rel='stylesheet' type='text/css'>
 </head>
@@ -35,16 +35,16 @@
     </div>
 </header>
 
-<div id="content-wrapper" class="column-6 side-padding h-panel">
+<div id="content-wrapper" class="column-8 side-padding h-panel">
     <div id="main-body" class="h-panel">
-        <div class="column-8">Look! Something something something something look over there --></div>
-        <div class="column-4">Content</div>
-        <div class="column-4">Content Column 2</div>
+        <div class="column-12">Look! Something something something something look over there --></div>
+        <div class="column-6">Content</div>
+        <div class="column-6">Content Column 2</div>
     </div>
 </div>
 <footer id="footer-content" class="h-panel">
     <div class="side-padding">
-    <div id="footer-links" class="column-3">
+    <div id="footer-links" class="column-4">
         <ul>
             <li>
                 <a href="index.php">Home</a>
@@ -54,7 +54,7 @@
             </li>
         </ul>
     </div>
-    <div class="column-5">
+    <div class="column-8">
         IDK Some content
     </div></div>
 </footer>
